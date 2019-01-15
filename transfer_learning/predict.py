78e6c2ece0f503377cce53c861f89cf5a52568ee
gaohang <gaohang0911@qq.com>
2019-01-15 14:25:13 +0800

    修改预测文件的代码风格以符合pep8； 从训练的模型测量单张图像，符合预期；

--- transfer_learning/predict.py
+++ transfer_learning/predict.py
@@ -11,69 +11,68 @@ from keras.models import load_model
 from keras.applications.inception_v3 import preprocess_input
 
 
-target_size = (229, 229) #fixed size for InceptionV3 architecture
+target_size = (229, 229)  # fixed size for InceptionV3 architecture
 
 
 def predict(model, img, target_size):
-  """Run model prediction on image
-  Args:
-    model: keras model
-    img: PIL format image
-    target_size: (w,h) tuple
-  Returns:
-    list of predicted labels and their probabilities
-  """
-  if img.size != target_size:
-    img = img.resize(target_size)
+    """Run model prediction on image
+    Args:
+        model: keras model
+        img: PIL format image
+        target_size: (w,h) tuple
+    Returns:
+        list of predicted labels and their probabilities
+    """
+    if img.size != target_size:
+        img = img.resize(target_size)
 
-  x = image.img_to_array(img)
-  x = np.expand_dims(x, axis=0)
-  x = preprocess_input(x)
-  preds = model.predict(x)
-  return preds[0]
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    preds = model.predict(x)
+    return preds[0]
 
 
 def plot_preds(image, preds):
-  """Displays image and the top-n predicted probabilities in a bar graph
-  Args:
-    image: PIL image
-    preds: list of predicted labels and their probabilities
-  """
-  plt.imshow(image)
-  plt.axis('off')
+    """Displays image and the top-n predicted probabilities in a bar graph
+    Args:
+        image: PIL image
+        preds: list of predicted labels and their probabilities
+    """
+    plt.imshow(image)
+    plt.axis('off')
 
-  plt.figure()
-  labels = ("cat", "dog")
-  plt.barh([0, 1], preds, alpha=0.5)
-  plt.yticks([0, 1], labels)
-  plt.xlabel('Probability')
-  plt.xlim(0,1.01)
-  plt.tight_layout()
-  plt.show()
+    plt.figure()
+    labels = ("cat", "dog")
+    plt.barh([0, 1], preds, alpha=0.5)
+    plt.yticks([0, 1], labels)
+    plt.xlabel('Probability')
+    plt.xlim(0, 1.01)
+    plt.tight_layout()
+    plt.show()
 
 
-if __name__=="__main__":
-  a = argparse.ArgumentParser()
-  #a.add_argument("--image", help="path to image", default=r"F:\DL\data\overlap\validate\Background\196(9).jpg")
-  a.add_argument("--image", help="path to image", default=r"F:\DL\data\overlap\validate\Exudate\212(73).jpg")
-  
-  a.add_argument("--image_url", help="url to image")
-  a.add_argument("--model", default=r"C:\Users\gaohang\Desktop\DeepLearningSandbox-master\transfer_learning\inceptionv3-ft.model")
-  args = a.parse_args()
+if __name__ == "__main__":
+    a = argparse.ArgumentParser()
+    # a.add_argument("--image", help="path to image", default=r"F:\DL\data\overlap\validate\Background\196(9).jpg")
+    a.add_argument("--image", help="path to image", default=r"F:\DL\data\cord\test1\56.jpg")
 
-  if args.image is None and args.image_url is None:
-    a.print_help()
-    sys.exit(1)
+    a.add_argument("--image_url", help="url to image")
+    a.add_argument("--model", default=r"C:\Users\gaohang\Desktop\DeepLearningSandbox\transfer_learning\inceptionv3-ft.model")
+    args = a.parse_args()
 
-  model = load_model(args.model)
-  if args.image is not None:
-    img = Image.open(args.image)
-    preds = predict(model, img, target_size)
-    plot_preds(img, preds)
+    if args.image is None and args.image_url is None:
+        a.print_help()
+        sys.exit(1)
 
-  if args.image_url is not None:
-    response = requests.get(args.image_url)
-    img = Image.open(BytesIO(response.content))
-    preds = predict(model, img, target_size)
-    plot_preds(img, preds)
+    model = load_model(args.model)
+    if args.image is not None:
+        img = Image.open(args.image)
+        preds = predict(model, img, target_size)
+        plot_preds(img, preds)
 
+    if args.image_url is not None:
+        response = requests.get(args.image_url)
+        img = Image.open(BytesIO(response.content))
+        preds = predict(model, img, target_size)
+        plot_preds(img, preds)
